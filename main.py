a4d60c35ba258aaf4cb4e6f6e3da6582aed59476
EleanorCordelia <bryan.rich0604@gmail.com>
2023-12-31 15:06:58 +0700

    [add] initiate functions

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+def constraint(x):
+    pass
+
+def sharpe(x):
+    pass
+
+def obj_function(x):
+    return sharpe(x) + 100 * constraint(x)
+
+if __name__ == "__main__":
+    pass
